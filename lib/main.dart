97a355490eea0628d991543f750562b9feafada9
CantykaLaily-231401004 <cantykalaily@students.usu.ac.id>
2025-11-06 16:19:41 +0700

    feat(main): integrate QuizProvider with MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'screens/splash_screen.dart';
+import 'providers/quiz_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +12,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SmartQuiz',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF4A70A9)),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (_) => QuizProvider(),
+      child: MaterialApp(
+        title: 'SmartQuiz',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF4A70A9)),
+          useMaterial3: true,
+          fontFamily: 'Poppins',
+        ),
+        home: const SplashScreen(),
       ),
-      home: const SplashScreen(),
     );
   }
 }
