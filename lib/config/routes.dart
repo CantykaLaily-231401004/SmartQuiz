a3ffc2b02041514c4249b4b635e6db08e3569725
CantykaLaily-231401004 <cantykalaily@students.usu.ac.id>
2025-11-06 21:50:18 +0700

    feat(home): implement home screen

---
+++ lib/config/routes.dart
@@ -0,0 +1,9 @@
+class AppRoutes {
+  static const String splash = '/';
+  static const String home = '/home';
+  static const String quiz = '/quiz';
+  static const String result = '/result';
+  static const String review = '/review';
+  static const String leaderboard = '/leaderboard';
+}
+
